571a628ec12d45949cf56b5beb4ec2b2036ab1f8
Shevchuksergiy <DerZoldaten@gmail.com>
2025-02-10 23:47:14 +0200

    Emailtest

--- tests/openloginPage.spec.ts
+++ tests/openloginPage.spec.ts
@@ -7,6 +7,7 @@ test('Button visibility test', async ({ page }) => {
     const contineWithLoginButton = page.locator ('//button/span[contains(.,"Continue with Email")]');
     const inputEmail = page.locator ('//input[@placeholder="name@mail.com"]');
     const errorMessage = page.locator ('//div[@data-testid="toast-content"]');
+    const errorMessage2 = page.locator ('//div[@id="input-1-messages"]');
 
     await page.goto('https://yard-noty.web.app/');
     await expect(contineWithGooglButton).toBeVisible();
@@ -14,11 +15,24 @@ test('Button visibility test', async ({ page }) => {
     await expect(inputEmail).toBeVisible();
     await (contineWithLoginButton).click();
 
-     // Ждем 0.5 секунды перед кликом
-     await page.waitForTimeout(1500);
-      // Проверяем, что появилось сообщение об ошибке
+    // Ждем 0.5 секунды перед кликом
+    await page.waitForTimeout(500);
     await expect(errorMessage).toBeVisible();
 
+    // Вводим текст в поле рабочий email
+    await inputEmail.fill('test@example.com');
+    await (contineWithLoginButton).click();
+    await page.waitForTimeout(5000);
+    await expect(errorMessage).toBeVisible();
+
+    // Вводим текст в поле кривой email
+    await inputEmail.fill('aAbB11');
+    await (contineWithLoginButton).click();
+    await expect(errorMessage2).toBeVisible();
+
+    
+
+    
     
   
    
