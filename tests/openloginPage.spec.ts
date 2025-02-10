29e2f371bd85f28eb2b6ec28781df76df6c1065b
Shevchuksergiy <DerZoldaten@gmail.com>
2025-02-10 21:46:55 +0200

    rrorMessage

--- tests/openloginPage.spec.ts
+++ tests/openloginPage.spec.ts
@@ -6,11 +6,21 @@ test('Button visibility test', async ({ page }) => {
     const contineWithGooglButton = page.locator ('//button/span[contains(.,"Continue with Google")]');
     const contineWithLoginButton = page.locator ('//button/span[contains(.,"Continue with Email")]');
     const inputEmail = page.locator ('//input[@placeholder="name@mail.com"]');
+    const errorMessage = page.locator ('//div[@data-testid="toast-content"]');
 
     await page.goto('https://yard-noty.web.app/');
     await expect(contineWithGooglButton).toBeVisible();
     await expect(contineWithLoginButton).toBeVisible();
     await expect(inputEmail).toBeVisible();
+    await (contineWithLoginButton).click();
+
+     // Ждем 0.5 секунды перед кликом
+     await page.waitForTimeout(1500);
+      // Проверяем, что появилось сообщение об ошибке
+    await expect(errorMessage).toBeVisible();
+
+    
   
+   
     
   });
